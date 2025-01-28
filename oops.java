e6a6f252ad9b747bfb05d6047157d5b79f088d2d
akash ln <akashln357@gmail.com>
2025-01-28 11:43:53 +0530

    jgvhg

---
+++ oops.java
@@ -0,0 +1,23 @@
+public class oops{
+    public static void main(String[]args)
+    {
+     student s1=new student();
+     student s2=new student("akash");
+     student s3=new student(234);
+    
+    }
+}
+class student{
+    String name;
+    int roll;
+    student(){
+        System.out.println("sdhvbsdjhbd");
+    }
+    student(String name){
+        this.name=name;
+        System.out.println();
+    }
+    student(int roll){
+        this.roll=roll;
+    }
+} 

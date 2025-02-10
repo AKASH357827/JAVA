439f6548757990ecd8322a2684b905e85c83a828
akash ln <akashln357@gmail.com>
2025-02-10 11:37:32 +0530

    sumrecursion

---
+++ sum.java
@@ -0,0 +1,15 @@
+public  class sum{
+    public static int sums(int n) {
+      
+      if(n==0){
+        return 0;
+      }
+    n=n+sums(n-1);
+    return n;
+    }
+    public static void main(String[] args) {
+        int n=10;
+        sums(n);
+        System.out.println(sums(n));
+    }
+}

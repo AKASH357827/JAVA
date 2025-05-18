c4d51a43599f5f4f96ecda2b730f72760b678ac9
akash ln <akashln357@gmail.com>
2025-05-18 13:12:45 +0530

    largeestnumber

--- practicing.java
+++ practicing.java
@@ -1,14 +1,13 @@
 public class practicing {
-    public static void main(String[] args) {
-        int rows = 6; 
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                
-                System.out.print((i + j) % 2);
+    public static void main(String []args) {
+        int a[]={1,2,6,3,6,22,5,2};
+        int large=0;
+        int i=0;
+        for(i=0;i<a.length-1;i++){
+            if(large<a[i]){
+                large=a[i];
             }
-            System.out.println();
         }
+        System.out.println("the largest number is "+large);
     }
 }
-
-

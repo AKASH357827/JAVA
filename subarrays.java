029e21754d5e2ed658a911ad01745846bfd91eaf
akash ln <akashln357@gmail.com>
2025-06-15 11:25:48 +0530

    sub

--- subarrays.java
+++ subarrays.java
@@ -1,16 +1,14 @@
 public class subarrays{
-    public static void main(String[]args){
-        int n[]={1,2,3,4,5,6};
-        for(int i=0;i<n.length;i++){
-        int start=i;
-        for(int j=i;j<n.length;j++){
-        int end=j;
-        for(int k=start;k<=end;k++){
-        System.out.print(n[k]+" ");
-        }
-        System.out.println();
-        }
-        System.out.println();
-       }
-       }
+    public static void sub(int a[]){
+     for(int i=0;i<a.length;i++){
+        for(int j=i+1;j<a.length;j++){
+            System.out.print("("+a[i]+""+a[j]+")");
     }
+    System.out.println("");
+}
+}
+    public static void main(String[] args) {
+        int a[]={2,4,6,8,10};
+        sub(a);
+    }
+}

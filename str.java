e904422cf4d488d73920155b23d7d96b6522fa50
akash ln <akashln357@gmail.com>
2025-08-04 11:33:41 +0530

    directions

---
+++ str.java
@@ -0,0 +1,30 @@
+
+
+public class str{
+    public static void main(String[] args) {
+      int x=0;
+      int y=0;
+      String path="WNEENESENNN";
+       for(int i=0;i<path.length();i++){
+        char dir=path.charAt(i);
+        if(dir=='S'){
+            y--;
+        }
+        else if(dir=='N'){
+            y++;
+        }
+        else if(dir=='W'){
+            x--;
+        }
+        else{
+            x++;
+        }
+        
+        
+       }
+       int x1=x*x;
+        int y1=y*y;
+        System.out.println((float)Math.sqrt(x1+y1));
+    }
+}
+

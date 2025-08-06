705420c60dac3111a4465dd35dde928dc10564d1
akash ln <akashln357@gmail.com>
2025-08-06 10:50:28 +0530

    hi

--- j.java
+++ j.java
@@ -1,23 +1,27 @@
-public class j{
-    public static void sel(int a[]){
-       for(int i=0;i<a.length;i++){
-        
-       }
+
+public class j {
+
+    public static void sel(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+
+        }
     }
+
     public static void main(String[] args) {
         int[][] matrix = {
             {1, 2, 3},
             {4, 5, 6},
             {7, 8, 9}
         };
-        int key=8;
-        for(int i=0;i<matrix.length;i++)
-        for(int j=0;j<matrix.length;j++){
-        {
-            if(key==matrix[i][j]){
-                System.out.println("found"+i+""+j);
+        int key = 8;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                {
+                    if (key == matrix[i][j]) {
+                        System.out.println("found" + i + "" + j);
+                    }
+                }
             }
         }
-    }
     }
 }

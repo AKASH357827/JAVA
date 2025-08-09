1832ef5284f3b3327bb08a53b3afb32bd59dbe4f
akash ln <akashln357@gmail.com>
2025-08-09 18:31:30 +0530

    sqrt

---
+++ sqrt.java
@@ -0,0 +1,11 @@
+public class sqrt {
+    public static int mySqrt(int x) {
+        int a = (int) Math.sqrt(x);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(8)); 
+    }
+}
+    

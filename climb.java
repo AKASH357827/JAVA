dd22a389b535756e3ab9dd4d220765494532c241
akash ln <akashln357@gmail.com>
2025-08-09 19:12:56 +0530

    climbing

---
+++ climb.java
@@ -0,0 +1,19 @@
+public class climb {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4)); 
+    }
+
+    public static int climbStairs(int n) {
+        if (n <= 2) return n; 
+
+        int first = 1;  
+        int second = 2; 
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second; 
+            first = second;
+            second = third;
+        }
+        return second; 
+    }
+}

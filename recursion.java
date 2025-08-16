de3da195726b49971960046022ac396df230b9c2
akash ln <akashln357@gmail.com>
2025-08-16 11:59:26 +0530

    hi

---
+++ recursion.java
@@ -0,0 +1,15 @@
+public class recursion{
+    public static void print(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        print(n-1);
+        System.out.println(n+" ");
+        
+    }
+    public static void main(String[] args) {
+       int n=3; 
+       print(n);
+    }
+}
